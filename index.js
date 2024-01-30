3e43748c797d3b7ab9e2ca8815fcef4e911f1a2b
+ <+>
2024-01-30 03:12:31 -0800

    arrow functions are cool

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = () => {
+    const result = 2000 / 100;
+    return result;
+}
+const square = (base) => {
+    const result = base * base;
+    return result;
+}
+const add = (num1, num2) => {
+    const result = (num1 + num2);
+    return result;
+}
